898409a6a6a84c236cebd8a4f0a0310277baa511
ritwikmath <ritwikmath@gmail.com>
2025-01-08 14:35:07 +0530

    Changed the process of identifying APP_ROOT directory

--- src/config/app.py
+++ src/config/app.py
@@ -1,14 +1,20 @@
 import os
 import sys
 from core.interfaces import ConfigInterface
+from pathlib import Path
 
 
 class AppConfig(ConfigInterface):
+    def __init__(self):
+        self.file_path = Path(__file__).resolve()
+
     def get_configurations(self) -> object:
+        file_path = Path(__file__).resolve()
+
         return {
             "APP_NAME": os.getenv("APP_NAME", "API_FIRST"),
             "ENV": os.getenv("ENV", "staging"),
             "DEBUG": os.getenv("DEBUG", False),
             "CIPHER": os.getenv("CIPHER", "Base64"),
-            "APP_ROOT": os.path.abspath(sys.modules["__main__"].__file__.rstrip("lambda_function.py"))
+            "APP_ROOT": self.file_path.parent.parent
         }
