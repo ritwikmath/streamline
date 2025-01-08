64e633f4a30893f381b8414a623bd4e48e66ff54
ritwikmath <ritwikmath@gmail.com>
2025-01-08 14:28:24 +0530

    Added general exception handling

--- src/core/wrappers.py
+++ src/core/wrappers.py
@@ -5,12 +5,16 @@ import json
 def parse_event(method):
     @wraps(method)
     def inner_function(self, *args):
-        event, context = args[0], args[1]
-        request_context = event["requestContext"]
-        http_method = request_context["http"]["method"]
-        http_path = request_context["http"]["path"]
-        body = json.loads(event.get("body"))
+        try:
+            event, context = args[0], args[1]
+            request_context = event["requestContext"]
+            http_method = request_context["http"]["method"]
+            http_path = request_context["http"]["path"]
+            body = json.loads(event.get("body"))
 
-        return method(self, http_path, http_method, body)    
+            return method(self, http_path, http_method, body)
+        except Exception as ex:
+            print(ex)
+            return "Something went wrong"
 
     return inner_function
