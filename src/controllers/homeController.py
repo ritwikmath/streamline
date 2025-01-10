a0d2e314180a8740b67e2fb3ec439f27cde900c1
ritwikmath <ritwikmath@gmail.com>
2025-01-10 12:20:11 +0530

    cleared some of the code

--- src/controllers/homeController.py
+++ src/controllers/homeController.py
@@ -1,48 +1,14 @@
 from core.application import app
-# from sqlalchemy import select
-# from database.models.userAccount import User
-# from sqlalchemy import update
-from validators.UserValidator import UserValidator
 
 
 @app.router.get('/simple/{var}')
 def simple(var):
-    # with app.db.client() as session:
-    #     stmt = select(User)
-    #     response = session.scalars(stmt)
-    #     session.close()
-    # app.cache.store_value("surname", "math")
-    # app.logger.info(app.cache.get_value("surname"))
-    # app.logger.info(response)
-    return 'result from the fn', 200
+    return {"param": var}, 200
 
 
 @app.router.post('/simple')
 def simple_store():
     payload = app.request.json
-    # app.cache.store_value("surname", "math")
-    # app.logger.info(app.cache.get_value("surname"))
-    # with app.db.client() as session:
-    #     validated_user = UserValidator(**payload)
-    #     spongebob = User(**validated_user.model_dump())
-    #     session.add(spongebob)
-    #     query = select(User)
-    #     for user in session.scalars(query):
-    #         app.logger.info(user)
-    #     response = f"{spongebob}"
-    #     print(response)
-    #     session.commit()
-    #     session.close()
-    return {"message": payload}, 200
-
-
-@app.router.patch('/simple/{var}')
-def simple_update(var):
-    payload = app.request.json
-    with app.db.client() as session:
-        spongebob = session.execute(select(User).filter_by(id=int(var))).scalar_one()
-        spongebob.name = payload["name"]
-        response = f"{spongebob}"
-        session.commit()
-        session.close()
-    return response
+    app.cache.store_value("surname", "math")
+    app.logger.info(app.cache.get_value("surname"))
+    return {"message": payload, "surname": app.cache.get_value("surname")}, 200
